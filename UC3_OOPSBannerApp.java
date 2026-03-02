569d1efbe080970a4f9373b13bec5e740e4ae753
Vaishnavi Tripathicd <tripathiaashi68@gmail.com>
2026-03-02 20:11:19 +0530

    UC3: OOPS banner using String.join()

---
+++ UC3_OOPSBannerApp.java
@@ -0,0 +1,13 @@
+public class UC3_OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", " *****   *****   *****   ***** "));
+        System.out.println(String.join("", "*     * *     * *     * *     *"));
+        System.out.println(String.join("", "*     * *     * *     * *     *"));
+        System.out.println(String.join("", "*     * *     * *     * *     *"));
+        System.out.println(String.join("", "*     * *     * *     * *     *"));
+        System.out.println(String.join("", "*     * *     * *     * *     *"));
+        System.out.println(String.join("", " *****   *****   *****   ***** "));
+    }
+}
